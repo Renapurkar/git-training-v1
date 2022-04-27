0d8d8e9681edff10aa7621619ff62bf8feb93bb6
KishorK <truecallerca@gmail.com>
2022-04-27 09:50:37 +0530

    My 2nd Git commit

---
+++ ex2Dfuncation.py
@@ -0,0 +1,16 @@
+#1) Offer letter with current employer / last appraisal letter reflecting compensation details
+#
+#2) Copies of 3 recent consecutive months salary slips
+#
+#3) Any documents relevant to Variable pay received
+#
+#4) Any document relevant to stocks etc
+#
+#5) Updated CV
+#
+#6) 2 attached excel sheets duly filled in with requisite details:
+#
+#a. Capgemini Documentation form – personal and professional details requested
+#
+#b. Current Salary breakup – existing salary structure with current organization to be filled
+#Fist Change to file
